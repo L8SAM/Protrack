2efdd525ebc1d35340d4964bc66b7c5c9df57cf1
L8SAM <222952795+L8SAM@users.noreply.github.com>
2026-02-07 18:34:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-alert("JS OK – vor Firebase");
+alert("1️⃣ JS gestartet");
 
-console.log("Firebase vorhanden?", typeof firebase);
+if (typeof firebase === "undefined") {
+  alert("❌ Firebase undefined");
+} else {
+  alert("✅ Firebase vorhanden");
+}
+
+/* TEST: NUR INIT */
+try {
+  firebase.initializeApp({
+    apiKey: "TEST",
+    projectId: "TEST"
+  });
+  alert("✅ firebase.initializeApp OK");
+} catch (e) {
+  alert("❌ INIT FEHLER: " + e.message);
+}
